eb5427a7ed0c7f23ef83d5b78c031b539a4bc5fa
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2026-09-14 21:27:32 +0300

    Add StudentBuilder stub with method signatures

--- src/main/java/com/sortingapp/model/StudentBuilder.java
+++ src/main/java/com/sortingapp/model/StudentBuilder.java
@@ -1,4 +1,19 @@
 package com.sortingapp.model;
 
 public class StudentBuilder {
+    public StudentBuilder groupNumber(String groupNumber) {
+        throw new UnsupportedOperationException("StudentBuilder.groupNumber not implemented yet");
+    }
+
+    public StudentBuilder averageScore(double averageScore) {
+        throw new UnsupportedOperationException("StudentBuilder.averageScore not implemented yet");
+    }
+
+    public StudentBuilder recordBookNumber(int recordBookNumber) {
+        throw new UnsupportedOperationException("StudentBuilder.recordBookNumber not implemented yet");
+    }
+
+    public Student build() {
+        throw new UnsupportedOperationException("StudentBuilder.build not implemented yet");
+    }
 }
