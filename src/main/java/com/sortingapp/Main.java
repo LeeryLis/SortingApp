a052b63f911a008899119eb4e5408ae87bbac321
silver14056 <silver14056@mail.ru>
2026-09-16 21:01:51 +0300

    LS: Test PullRequest

--- src/main/java/com/sortingapp/Main.java
+++ src/main/java/com/sortingapp/Main.java
@@ -5,5 +5,9 @@ import com.sortingapp.model.Student;
 public class Main {
     static void main() {
         System.out.println("Sorting App");
+
+        System.out.println("Меня зовут Сергей. Я форкнул проект из репозитория Кирила. Клонировал себе на комп. " +
+                "Создал ветку testBranchPakepko. В ней добавил этот sout. Сейчас сделаю коммит и пуш. " +
+                "Затем отправлю PullRequest Кириллу!");
     }
 }
