adf6e7dd1441513ccbba014cc0ab2640b3b60e0f
Maslina <trigunpichita@gmail.com>
2026-09-19 20:04:08 +0300

    Cоздаёт в формате "чтобы были" через случайный генератор поля студента и добавляет в лист count число студентов через StudentBuilder. На данный момент имеется неопределённость формата полей.

--- src/main/java/com/sortingapp/util/RandomFiller.java
+++ src/main/java/com/sortingapp/util/RandomFiller.java
@@ -1,16 +1,46 @@
 package com.sortingapp.util;
 
+import com.sortingapp.model.StudentBuilder;
 import com.sortingapp.model.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public final class RandomFiller {
+
+    //Cоздаёт в формате "чтобы были" через случайный генератор поля студента
+    // и добавляет в лист count число студентов через StudentBuilder.
+    //На данный момент имеется проблема формата полей.
+
+
     private static final Random RANDOM = new Random();
 
     private RandomFiller() {}
 
+    private static String getRandomGroupNumber() {
+        return String.valueOf(RANDOM.nextInt(100001));
+    }
+
+    private static double getRandomAverageScore() {
+        return (RANDOM.nextInt(401) + 100) / 100.0;
+    }
+
+    private static int getRandomRecordBookNumber() {
+        return RANDOM.nextInt(100001)+1;
+    }
+
     public static List<Student> generateStudents(int count) {
-        return List.of();
+
+        List<Student> students = new ArrayList<>(count);
+        for(int i =0; i < count; i++) {
+            students.add(new StudentBuilder()
+                    .groupNumber(getRandomGroupNumber())
+                    .averageScore(getRandomAverageScore())
+                    .recordBookNumber(getRandomRecordBookNumber())
+                    .build());
+        }
+
+        return List.copyOf(students);
     }
 }
