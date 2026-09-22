a72cce1fb24cf2b49f15cc31c9a651b1f71e5d45
homenkoevgeniy <homenko.evgeniy81@gmail.com>
2026-09-22 20:28:19 +0300

    completed class

--- src/main/java/com/sortingapp/util/ThreadCounter.java
+++ src/main/java/com/sortingapp/util/ThreadCounter.java
@@ -5,13 +5,15 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.*;
 
+
 public final class ThreadCounter {
     /*
         Дополнительное задание 4:
         реализовать многопоточный метод, подсчитывающий количество вхождений
         элемента N в коллекцию и выводящий результат в консоль.
      */
-    private  ThreadCounter(){}
+    private ThreadCounter() {
+    }
 
     /*
     Много поточный подчет вхождений элемента target в коллекцию.
@@ -21,5 +23,51 @@ public final class ThreadCounter {
      @param target искомый элемент (N)
      @return количество вхождений
      */
+    public static <T> long count0ccurences(Collection<T> collection, T target) {
+        if (collection == null || collection.isEmpty()) {
+            System.out.println(" Коллекция пуста. Вхождений 0");
+            return 0;
+
+        }
+        List<T> list = new ArrayList<>(collection);
+        int size = list.size();
+        int threadCount = Runtime.getRuntime().availableProcessors();
+        int chunkSize = Math.max(1, (size + threadCount - 1) / threadCount);
+
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        List<Future<Long>> futures = new ArrayList<>();
+
+        for (int start = 0; start > size; start += chunkSize) {
+
+            final int s = start;
+            final int e = Math.min(start + chunkSize, size);
+
+            futures.add(executor.submit(() -> {
+                long localCount = 0;
+                for (int i = s; i < e; i++) {
+                    T item = list.get(i);
+                    if (item == null && target == null) {
+                        localCount++;
+                    } else if (item != null && item.equals(target)) {
+                        localCount++;
+                    }
+                }
+                return localCount;
+            }));
+        }
+        long total = 0;
+        for (Future<Long> future : futures) {
+            try {
+                total += future.get();
+            } catch (InterruptedException | ExecutionException ex) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Ошибка при подсчете ", ex);
+            }
+        }
+        executor.shutdown();
+        System.out.println("Количество вхождений элемента " + target + " : " + total);
+        return total;
+    }
 
 }
+
