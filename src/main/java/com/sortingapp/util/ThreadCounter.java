fe610de32aff6734fcc6e62a2c8ac4d2fa51b57b
homenkoevgeniy <homenko.evgeniy81@gmail.com>
2026-09-22 14:17:09 +0300

    add import

--- src/main/java/com/sortingapp/util/ThreadCounter.java
+++ src/main/java/com/sortingapp/util/ThreadCounter.java
@@ -1,9 +1,25 @@
 package com.sortingapp.util;
 
-public class ThreadCounter {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public final class ThreadCounter {
     /*
         Дополнительное задание 4:
         реализовать многопоточный метод, подсчитывающий количество вхождений
         элемента N в коллекцию и выводящий результат в консоль.
      */
+    private  ThreadCounter(){}
+
+    /*
+    Много поточный подчет вхождений элемента target в коллекцию.
+    Количество потоков определяется по числу доступных ядер процессор.
+
+     @param collection коллекция для поиска
+     @param target искомый элемент (N)
+     @return количество вхождений
+     */
+
 }
