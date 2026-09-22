2bb39c81b726067ad8357019a0a418cc0ffbe0d3
Isaeva Alyona <ai2799@yandex.ru>
2026-09-22 22:06:31 +0500

    Сортировки + доп. задание 1

--- src/main/java/com/sortingapp/sort/algorithms/EvenOddRecordBookSort.java
+++ src/main/java/com/sortingapp/sort/algorithms/EvenOddRecordBookSort.java
@@ -5,6 +5,7 @@ import com.sortingapp.sort.SortStrategy;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
 
 public class EvenOddRecordBookSort implements SortStrategy {
     private final SortStrategy inner;
@@ -13,18 +14,38 @@ public class EvenOddRecordBookSort implements SortStrategy {
         this.inner = inner;
     }
 
+    /*
+        Дополнительное задание 1:
+        дополнительно к основным сортировкам реализовать эти же алгоритмы
+        сортировки таким образом, что объекты классов будут сортироваться
+        по какому-либо числовому полю: объекты с четными значениями этого поля
+        должны быть отсортированы в натуральном порядке, а с нечетными – оставаться
+        на исходных позициях.
+
+        Поле для сортировки:
+        Номер зачётной книжки (recordBookNumber)
+     */
     @Override
     public void sort(List<Student> students, Comparator<Student> comparator) {
-        /*
-            Дополнительное задание 1:
-            дополнительно к основным сортировкам реализовать эти же алгоритмы
-            сортировки таким образом, что объекты классов будут сортироваться
-            по какому-либо числовому полю: объекты с четными значениями этого поля
-            должны быть отсортированы в натуральном порядке, а с нечетными – оставаться
-            на исходных позициях.
+        List<Student> tempStudents = new ArrayList<>();
+        List<Boolean> evenElements = new ArrayList<>();
+        for (Student s : students) {
+            if (s.getRecordBookNumber() % 2 == 0) {
+                tempStudents.add(s);
+                evenElements.add(true);
+            } else {
+                evenElements.add(false);
+            }
+        }
 
-            Поле для сортировки:
-            Номер зачётной книжки (recordBookNumber)
-         */
+        inner.sort(tempStudents, comparator);
+
+        int j = 0;
+        for (int i = 0; i < students.size(); i++) {
+            if (evenElements.get(i) == true) {
+                students.set(i, tempStudents.get(j));
+                j++;
+            }
+        }
     }
 }
