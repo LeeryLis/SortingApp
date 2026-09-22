81f7360a24570065ed94f03527ea845c09765e6e
Maslina <trigunpichita@gmail.com>
2026-09-22 10:24:18 +0300

    Вывел приватный метод наверх Убрал некоторые пробелы

--- src/main/java/com/sortingapp/model/StudentBuilder.java
+++ src/main/java/com/sortingapp/model/StudentBuilder.java
@@ -3,7 +3,6 @@ package com.sortingapp.model;
 public class StudentBuilder {
 
     //Это билдер класса Student.
-    //Он создаёт объект Student.
     //Переменные, оканчивающиеся на Set, нужны, чтобы убедится, что все поля были заполнены.
 
     private String groupNumber;
@@ -14,8 +13,14 @@ public class StudentBuilder {
     private boolean averageScoreSet;
     private boolean recordBookNumberSet;
 
-    public StudentBuilder groupNumber(String groupNumber) {
+    public Student build() {
+        if (!groupNumberSet || !averageScoreSet || !recordBookNumberSet) {
+            throw new IllegalStateException( "All student parameters must be specified" );
+        }
+        return new Student( groupNumber, averageScore, recordBookNumber );
+    }
 
+    public StudentBuilder groupNumber(String groupNumber) {
         if (groupNumber == null || groupNumber.isBlank()) {
             throw new IllegalArgumentException( "Group number cannot be empty" );
         }
@@ -25,23 +30,14 @@ public class StudentBuilder {
     }
 
     public StudentBuilder averageScore(double averageScore) {
-
         this.averageScore = averageScore;
         this.averageScoreSet = true;
         return this;
     }
 
     public StudentBuilder recordBookNumber(int recordBookNumber) {
-
         this.recordBookNumber = recordBookNumber;
         this.recordBookNumberSet = true;
         return this;
-    }
-
-    public Student build() {
-        if (!groupNumberSet || !averageScoreSet || !recordBookNumberSet) {
-            throw new IllegalStateException( "All student parameters must be specified" );
-        }
-        return new Student( groupNumber, averageScore, recordBookNumber );
     }
 }
