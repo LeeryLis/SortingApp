8db4a6331ae63e9d259115bf9c3024ea9369bb39
Maslina <trigunpichita@gmail.com>
2026-09-23 19:35:12 +0300

    студанта отпустило

--- src/test/java/model/BuildTest.java
+++ src/test/java/model/BuildTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BuildTest {
 @Test
 public void testBuilder(){
-    Student student = Student.builder().groupNumber("1488").averageScore(3).recordBookNumber(228).build();
-    assertEquals("1488", student.getGroupNumber());
+    Student student = Student.builder().groupNumber("14367").averageScore(3).recordBookNumber(6267).build();
+    assertEquals("14367", student.getGroupNumber());
     assertEquals(3, student.getAverageScore());
-    assertEquals(228, student.getRecordBookNumber());
+    assertEquals(6267, student.getRecordBookNumber());
 }
 }
