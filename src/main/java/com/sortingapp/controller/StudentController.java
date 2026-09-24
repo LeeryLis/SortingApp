31ca05d8a9f7d12bd4e52885fda0a3d4273c2127
silver14056 <silver14056@mail.ru>
2026-09-24 17:32:07 +0300

    Refactor StudentController: dell comment

--- src/main/java/com/sortingapp/controller/StudentController.java
+++ src/main/java/com/sortingapp/controller/StudentController.java
@@ -81,13 +81,11 @@ public class StudentController {
     }
 
     private void sort() {
-        if(students.isEmpty()) {
+        if (students.isEmpty()) {
             view.showError("Список пуст — нечего сортировать.");
             return;
         }
 
-//        List<Student> sortedStudents = new ArrayList<>(students);
-//
         Comparator<Student> comparator = chooseSortField(view.showFieldMenuAndAsk());
         SortStrategy sortStrategy = chooseSortStrategy(view.showSortStrategyMenuAndAsk());
 
